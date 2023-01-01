58063d617baa0ff71f761a6ca7ecfca06a4b295f
Jie Chen <113589366+JiwChe@users.noreply.github.com>
2023-01-01 16:58:06 +0800

    pull test

--- src/main/java/com/cj/git/GitTest.java
+++ src/main/java/com/cj/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master git2!");
         System.out.println("dev hello git2!");
         System.out.println("push test!");
+        System.out.println("pull test!"); 
     }
 }
